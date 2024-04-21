30413a7f899a9f4f80ffc14b4a1378fa22a1ac93
patriciauhh <152938408+patriciauhh@users.noreply.github.com>
2024-04-21 18:25:39 -0400

    Add files via upload

---
+++ samplenotes.py
@@ -0,0 +1,26 @@
+
+#saving and reading text files
+#- "r" opens the text file for reading 
+#- "w" is for writing over existing files and is modified 
+#- first line is to create a new file 
+#- ".write" is to insert the string into the textfile 
+#created
+#- ".readlines" returns what's on the file then returns it
+
+
+#The child class inherits the constructor of their parent class, which allows the child class to reuse the initialization from their parent class.
+
+#OOP helps us organize our code into neatlittle packages, making it easier to understand and work with. It's used in lots of different places, from making video games to building websites and everything in between.
+#For example, 
+
+#class BlackjackGame:  # initializes a deck of cards 
+    #def __init__(self, suits, ranks, values, nplayers):
+        #self.suits = suits
+       #self.ranks = ranks
+        #self.values = values
+        #self.nplayers = nplayers
+        #self.deck = self.make_deck()
+        #random.shuffle(self.deck)
+        #self.player1_hand = [] # lists to store the cards in 
+        #self.dealer_hand = [] # lists to store the cards in 
+#this class is for the main blackjack game
